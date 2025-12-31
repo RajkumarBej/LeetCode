6044a19a2bda21db256708bd4bee9aafae5d863b
LxSuryaBadx <suryakantabar8260@gmail.com>
2025-12-31 02:21:19 -0500

    Time: 5 ms (83.49%), Space: 44.3 MB (21.6%) - LeetHub

--- 0001-can-make-arithmetic-progression-from-sequence/0001-can-make-arithmetic-progression-from-sequence.java
+++ 0001-can-make-arithmetic-progression-from-sequence/0001-can-make-arithmetic-progression-from-sequence.java
@@ -3,8 +3,7 @@ class Solution {
         Arrays.sort(arr);
         if(arr.length<=2) return true;
         int temp=Math.abs(arr[0]-arr[1]);
-        int i=1;
-        while(i<=(arr.length-2))
+        for(int i=1;i<=(arr.length-2);i++)
         {
             int x=Math.abs(arr[i]-arr[i+1]);
             if(x!=temp)return false;
