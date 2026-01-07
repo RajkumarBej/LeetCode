b3ca16df3d2783028a9cfbb591d28977cc0cbb1b
LxSuryaBadx <suryakantabar8260@gmail.com>
2026-01-07 02:29:00 -0500

    Time: 20 ms (9.35%), Space: 73.7 MB (32.79%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]<1)continue;
+            if(i<nums.length-1 && nums[i]==nums[i+1])continue;
             if(flag && nums[i]>1){
                 flag=false;
                 return 1;
