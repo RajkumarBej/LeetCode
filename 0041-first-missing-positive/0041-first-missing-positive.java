b24e9e4e526191e978577280a498dfb83bff8227
LxSuryaBadx <suryakantabar8260@gmail.com>
2026-01-07 02:27:51 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        boolean flag=true;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<1)continue;
+            if(flag && nums[i]>1){
+                flag=false;
+                return 1;
+            }
+            else flag=false;
+            if(i<nums.length-1 && nums[i]+1!=nums[i+1])return nums[i]+1;
+        }
+        if(nums[nums.length-1]<1)return 1;
+        return nums[nums.length-1]+1;
+    }
+}
