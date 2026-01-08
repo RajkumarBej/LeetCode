6f342940615f8fab2ba574b619675f15d0cac870
LxSuryaBadx <suryakantabar8260@gmail.com>
2026-01-08 05:24:46 -0500

    Time: 1 ms (58.89%), Space: 47.9 MB (47.63%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,7 +1,31 @@
 class Solution {
     public int trap(int[] height) {
+        int i=0,j=height.length-1,leftMax=0,rightMax=0,total=0;
+        while(i<j)
+        {
+            leftMax=Math.max(leftMax,height[i]);
+            rightMax=Math.max(rightMax,height[j]);
+
+            if(leftMax<rightMax)
+            {
+                total+=leftMax-height[i];
+                i++;
+            }
+            else
+            {
+                total+=rightMax-height[j];
+                j--;
+            }
+        }
+        return total;
+    }
+}
+
+/*First approach:-
+class Solution {
+    public int trap(int[] height) {
         int max=0,leftMax[]=new int[height.length],rightMax[]=new int[height.length];
-        for(int i=0;i<height.length;i++)
+        for(int i=0;i<height.length;i++)//find left max prefix
         {
             if(height[i]>=max)
             {
@@ -12,7 +36,7 @@ class Solution {
         }
 
         max=0;
-        for(int i=height.length-1;i>=0;i--)
+        for(int i=height.length-1;i>=0;i--)//find right max prefix
         {
             if(height[i]>=max)
             {
@@ -22,11 +46,11 @@ class Solution {
             else rightMax[i]=max;
         }
         int total=0;
-        for(int i=0;i<height.length;i++)
+        for(int i=0;i<height.length;i++)//than minimum between tham making contener and if we subtract it from our current height we can get how much water is stored in the current building and repeat this for every building.
         {
             total=total+(Math.min(leftMax[i],rightMax[i])-height[i]);
         }
 
         return total;
     }
-}
+} */
