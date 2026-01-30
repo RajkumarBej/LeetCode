a56ad35e9027fcad67da6a1340832f829158569d
LxSuryaBadx <suryakantabar8260@gmail.com>
2026-01-30 09:38:33 -0500

    Time: 1 ms (99.5%), Space: 47.4 MB (49.19%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,4 +1,22 @@
 class Solution {
+    public int jump(int[] nums) {
+        int l=0,r=0,jumps=0;
+        while(r<nums.length-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++)
+            {
+                farthest=Math.max(farthest,nums[i]+i);
+            }
+            l=r+1;
+            r=farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+}
+
+/*First Approach:-
+class Solution {
     int solution(int i,int[] a,int jumpCount){
         int minv=Integer.MAX_VALUE;
         if(i>=a.length-1){
@@ -13,3 +31,4 @@ class Solution {
         return solution(0,nums,0);
     }
 }
+ */
