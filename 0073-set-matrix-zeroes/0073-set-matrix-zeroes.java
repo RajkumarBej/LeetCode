89667c2cc3fdc4ed1fcc157741b6a595a7d3e1ac
LxSuryaBadx <suryakantabar8260@gmail.com>
2026-03-22 09:36:00 -0500

    Time: 142 ms (5.25%), Space: 51.5 MB (8.75%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,33 @@
+class Pair{
+    int row;
+    int col;
+    Pair(int row, int col){
+        this.row =row;
+        this.col=col;
+    }
+    public String toString(){return row+"-"+col;}
+}
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Map<Integer, Pair> map=new HashMap<>();
+
+        int count=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    map.put(count,new Pair(i,j));
+                    count++;
+                }
+            }
+        }
+        System.out.println(map);
+        for(Integer i:map.keySet()){
+            for(int j=0;j<matrix[0].length;j++)
+            matrix[map.get(i).row][j]=0;
+        }
+        for(Integer i:map.keySet()){
+            for(int j=0;j<matrix.length;j++)
+            matrix[j][map.get(i).col]=0;
+        }
+    }
+}
