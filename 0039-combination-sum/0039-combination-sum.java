c49d2125a53e9bc4c4abd691c29438c3d7840ade
LxSuryaBadx <suryakantabar8260@gmail.com>
2026-06-18 12:23:40 -0500

    Time: 3 ms (34.26%), Space: 45.9 MB (33.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,19 +1,17 @@
 class Solution {
     void combination(int []a,int idx,int t,List<Integer> list,List<List<Integer>> ans){
-        if(idx==a.length)
-        {
-            if(t==0)ans.add(new ArrayList(list));
+        if(idx>=a.length){
+            if(t==0)
+            ans.add(new ArrayList(list));
             return;
         }
 
-        if(a[idx]<=t)//if array index value is greater or equal to target, than we can consider it for sum
-        {
-            list.add(a[idx]);//insert the value into a temporary list
-            combination(a,idx,t-a[idx],list,ans);//find all combination of that index and other remaining indexes untill the target not equal to zero, if target not qual to zero after all posibility back track, means remove list last value and increase ind value.
-            list.remove(list.size()-1);
-        }
-        combination(a,idx+1,t,list,ans);
+        list.add(a[idx]);
+        if(a[idx]<=t)
+        combination(a,idx,t-a[idx],list,ans);
         
+        list.remove(list.size()-1);
+        combination(a,idx+1,t,list,ans);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         if(candidates.length==1)return Collections.emptyList();
@@ -23,3 +21,21 @@ class Solution {
         return ans;
     }
 }
+
+
+//  void combination(int []a,int idx,int t,List<Integer> list,List<List<Integer>> ans){
+//         if(idx==a.length)
+//         {
+//             if(t==0)ans.add(new ArrayList(list));
+//             return;
+//         }
+
+//         if(a[idx]<=t)//if array index value is greater or equal to target, than we can consider it for sum
+//         {
+//             list.add(a[idx]);//insert the value into a temporary list
+//             combination(a,idx,t-a[idx],list,ans);//find all combination of that index and other remaining indexes untill the target not equal to zero, if target not qual to zero after all posibility back track, means remove list last value and increase ind value.
+//             list.remove(list.size()-1);
+//         }
+//         combination(a,idx+1,t,list,ans);
+        
+//     }
